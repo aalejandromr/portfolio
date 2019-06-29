f4b5b0de6f73617a9ca63d93cb47793bf03f66b1
aalejandromr <aalejandro.mreyes@icloud.com>
2019-06-29 11:24:28 -0400

    Polish testimonials a bit more

--- src/Testimonials/index.js
+++ src/Testimonials/index.js
@@ -1,30 +1,68 @@
 import React from "react";
 import { Grid, Header, Icon } from "semantic-ui-react";
 import Lorem from "react-lorem-component";
+import styled from "styled-components";
 
 class Testimonials extends React.Component {
   render() {
     return (
-      <Grid columns={3} stackable divided>
-        <Grid.Row>
-          <Grid.Column textAlign="center" verticalAlign="middle">
-            <Lorem count={1} />
-            <Icon name="quote left" /> <Header as="h3"> Bojorquez </Header>
-          </Grid.Column>
+      <>
+        <Header as="h2"> Testimonials </Header>
+        <Grid columns={3} stackable divided>
+          <Grid.Row>
+            <Grid.Column textAlign="left" verticalAlign="middle">
+              <Grid columns={2}>
+                <Grid.Column width={2}>
+                  <Icon name="quote left" corner="bottom left" />
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  <Lorem count={1} />
+                </Grid.Column>
+              </Grid>
+              <StyledHeaderH3 as="h3"> Bojorquez </StyledHeaderH3>
+              <StyledHeaderH5 as="h5"> Telus International </StyledHeaderH5>
+            </Grid.Column>
 
-          <Grid.Column textAlign="center" verticalAlign="middle">
-            <Lorem count={1} />
-            <Icon /> <Header as="h3"> Bojorquez </Header>
-          </Grid.Column>
+            <Grid.Column textAlign="left" verticalAlign="middle">
+              <Grid columns={2}>
+                <Grid.Column width={2}>
+                  <Icon name="quote left" corner="bottom left" />
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  <Lorem count={1} />
+                </Grid.Column>
+              </Grid>
+              <StyledHeaderH3 as="h3"> Bojorquez </StyledHeaderH3>
+              <StyledHeaderH5 as="h5"> Telus International </StyledHeaderH5>
+            </Grid.Column>
 
-          <Grid.Column textAlign="center" verticalAlign="middle">
-            <Lorem count={1} />
-            <Icon /> <Header as="h3"> Bojorquez </Header>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+            <Grid.Column textAlign="left" verticalAlign="middle">
+              <Grid columns={2}>
+                <Grid.Column width={2}>
+                  <Icon name="quote left" corner="bottom left" />
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  <Lorem count={1} />
+                </Grid.Column>
+              </Grid>
+              <StyledHeaderH3 as="h3"> Bojorquez </StyledHeaderH3>
+              <StyledHeaderH5 as="h5"> Telus International </StyledHeaderH5>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </>
     );
   }
 }
+
+const StyledHeaderH3 = styled(Header)`
+  margin-bottom: 0;
+  padding-left: 5%;
+`;
+
+const StyledHeaderH5 = styled(Header)`
+  margin-top: 0;
+  padding-left: 5.5%;
+`;
 
 export default Testimonials;
