b0ad9f14db06cbeaf138c4b5293a0416c37ab39a
aalejandromr <aalejandro.mreyes@icloud.com>
2019-06-29 10:55:22 -0400

    Adds Testimonial layout

--- src/Testimonials/index.js
+++ src/Testimonials/index.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Grid, Header, Icon } from "semantic-ui-react";
+import Lorem from "react-lorem-component";
 
 class Testimonials extends React.Component {
   render() {
-    return <h1> Testimonials </h1>;
+    return (
+      <Grid columns={3} stackable divided>
+        <Grid.Row>
+          <Grid.Column textAlign="center" verticalAlign="middle">
+            <Lorem count={1} />
+            <Icon name="quote left" /> <Header as="h3"> Bojorquez </Header>
+          </Grid.Column>
+
+          <Grid.Column textAlign="center" verticalAlign="middle">
+            <Lorem count={1} />
+            <Icon /> <Header as="h3"> Bojorquez </Header>
+          </Grid.Column>
+
+          <Grid.Column textAlign="center" verticalAlign="middle">
+            <Lorem count={1} />
+            <Icon /> <Header as="h3"> Bojorquez </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
   }
 }
 
